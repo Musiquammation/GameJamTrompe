4f71f2f9386d93a01d0a867b3c3b2f1f1f902790
momeir <118108602+momeir@users.noreply.github.com>
2026-03-27 20:42:30 +0100

    t

--- src/Entity.py
+++ src/Entity.py
@@ -8,16 +8,23 @@ class Entity:
 	animations : list[Animation]
 	animation_index : int 
 
-	def __init__(self, x: float, y: float, _animations : list[Animation]):
+	def __init__(self, x: float, y: float, animations : list[Animation]):
 		self.x = x
 		self.y = y
+		self.animations = animations
+		self.animation_index = 0
 
 	def move(self, dx : float, dy : float) :
 		self.x += dx
 		self.y += dy
 
 	def set_current_animation(self, animation_name : str):
-		for i in range()
+		for i in range(len(self.animations)):
+			if self.animations[i].name == animation_name :
+				self.animation_index = i
+
+				
+				return
 	
 	def update(self, game : Game):
 		pass
