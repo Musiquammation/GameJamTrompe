2715484d646405eca32c58001dd3cc1987ec3a2f
Musiquammation <bryanmegherbi@gmail.com>
2026-03-27 20:27:34 +0100

    InputHandler

--- src/Game.py
+++ src/Game.py
@@ -1,8 +1,11 @@
 from pygame import Surface
 from Player import Player
+from InputHandler import InputHandler
 
 class Game:
 	player = Player(0,0)
+	inputHandler = InputHandler
+	frameCount = 0
 
 	def __init__(self):
 		pass
