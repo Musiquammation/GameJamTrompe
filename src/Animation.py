ec61b2954b7c3dde5962e82f1a228a2aefb87780
momeir <118108602+momeir@users.noreply.github.com>
2026-03-27 20:39:42 +0100

    animation et entity

--- src/Animation.py
+++ src/Animation.py
@@ -1,13 +1,17 @@
 from pygame import Rect
-
+from Game import Game
 
 class Animation:
+    name : str 
     frames : list[Rect]
     fps : float
     lastFrameStartTime : float
     frame_index : int = 0
 
-    def __init__(self, _frames : list[Rect] = [], _fps : float = 5):
-        frames = _frames
-        fps = _fps
+    def __init__(self, name : str = "Animation", frames : list[Rect] = [], fps : float = 5):
+        self.frames = frames
+        self.fps = fps
+        self.name = name
 
+    def update(self, game : Game):
+        pass
--- src/Entity.py
+++ src/Entity.py
@@ -1,18 +1,25 @@
 from pygame import Surface
+from Game import Game
+from Animation import Animation
 
 class Entity:
 	x: float
 	y: float
+	animations : list[Animation]
+	animation_index : int 
 
-	def __init__(self, x: float, y: float):
+	def __init__(self, x: float, y: float, _animations : list[Animation]):
 		self.x = x
 		self.y = y
 
 	def move(self, dx : float, dy : float) :
 		self.x += dx
 		self.y += dy
+
+	def set_current_animation(self, animation_name : str):
+		for i in range()
 	
-	def update(self):
+	def update(self, game : Game):
 		pass
 
 	def draw(self, screen : Surface):
